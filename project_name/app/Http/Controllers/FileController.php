74f6d1e56f6ebf3e71b9e3433e04457c797f654f
Ismail irfan <98257637+Ismailirfan@users.noreply.github.com>
2024-05-31 15:57:49 +0000

    I updated all these

--- project_name/app/Http/Controllers/FileController.php
+++ project_name/app/Http/Controllers/FileController.php
@@ -1,31 +1,4 @@
-<?php
-/*
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-
-class FileController extends Controller
-{
-    public function showFiles()
-    {
-        $fileUrls = [
-            'file1.html' => 'https://drive.google.com/uc?export=view&id=FILE_ID_1',
-            'file2.html' => 'https://drive.google.com/uc?export=view&id=FILE_ID_2'
-        ];
-
-        $fileContents = [];
-
-        foreach ($fileUrls as $fileName => $url) {
-            $response = Http::get($url);
-            $fileContents[$fileName] = $response->body();
-        }
-
-        return view('files', compact('fileContents'));
-    }
-}
-*/
-
+<?php 
 // app/Http/Controllers/FileController.php
 
 namespace App\Http\Controllers;
@@ -38,26 +11,23 @@ class FileController extends Controller
         {
                 // List of file IDs from the public Google Drive folder
                         $fileIds = [
-                          //  'file1' => 'YOUR_FILE_ID_1',
-                            'file2' => '126tUoG0Vt5cDSgwZY7J34xkVX73afstV',
-                                                            // Add more file IDs as needed
-                                                                    ];
+                                    'file1' => '126tUoG0Vt5cDSgwZY7J34xkVX73afstV',
+                                         //       'file2' => 'YOUR_FILE_ID_2',
+                                                     // Add more file IDs as needed
+                                                             ];
 
-                                                                            $files = [];
+                                                                     $files = [];
 
-                                                                                    foreach ($fileIds as $fileName => $fileId) {
-                                                                                                $fileUrl = "https://drive.google.com/uc?export=download&id=$fileId";
-                                                                                                            $fileContents = file_get_contents($fileUrl);
-                                                                                                                        $files[] = [
-                                                                                                                                        'name' => $fileName,
-                                                                                                                                                        'content' => $fileContents,
-                                                                                                                                                                    ];
-                                                                                                                                                                            }
+                                                                             foreach ($fileIds as $fileName => $fileId) {
+                                                                                         $fileUrl = "https://drive.google.com/uc?export=download&id={$fileId}";
+                                                                                                     $fileContents = file_get_contents($fileUrl);
+                                                                                                                 $files[] = [
+                                                                                                                                 'name' => $fileName,
+                                                                                                                                                 'content' => $fileContents,
+                                                                                                                                                             ];
+                                                                                                                                                                     }
 
-                                                                                                                                                                                    return view('files', compact('files'));
-                                                                                                                                                                                        }
-                                                                                                                                                                                        }
-
-
-?>
-  
+                                                                                                                                                                             return view('files', compact('files'));
+                                                                                                                                                                                 }
+                                                                                                                                                                                 }
+                                                                                                                                                                                 ?>
--- project_name/resources/views/files.blade.php
+++ project_name/resources/views/files.blade.php
@@ -4,35 +4,36 @@
 <html>
 <head>
     <title>Google Drive Files</title>
-        <style>
-                body {
-                            font-family: Arial, sans-serif;
-                                    }
-                                            .file-container {
-                                                        margin-bottom: 20px;
-                                                                }
-                                                                        .file-name {
-                                                                                    font-weight: bold;
-                                                                                                font-size: 1.2em;
-                                                                                                        }
-                                                                                                                .file-content {
-                                                                                                                            white-space: pre-wrap; /* To preserve formatting */
-                                                                                                                                        background-color: #f9f9f9;
-                                                                                                                                                    border: 1px solid #ddd;
-                                                                                                                                                                padding: 10px;
-                                                                                                                                                                        }
-                                                                                                                                                                            </style>
-                                                                                                                                                                            </head>
-                                                                                                                                                                            <body>
-                                                                                                                                                                                <h1>Files from Google Drive</h1>
-                                                                                                                                                                                    <div>
-                                                                                                                                                                                            @foreach($files as $file)
-                                                                                                                                                                                                        <div class="file-container">
-                                                                                                                                                                                                                        <div class="file-name">{{ $file['name'] }}</div>
-                                                                                                                                                                                                                                        <div class="file-content">{{ $file['content'] }}</div>
-                                                                                                                                                                                                                                                    </div>
-                                                                                                                                                                                                                                                            @endforeach
-                                                                                                                                                                                                                                                                </div>
-                                                                                                                                                                                                                                                                </body>
-                                                                                                                                                                                                                                                                </html>
-                                                                                                                                                                                                                                                                
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .file-container {
+            margin-bottom: 20px;
+        }
+        .file-name {
+            font-weight: bold;
+            font-size: 1.2em;
+        }
+        .file-content {
+            white-space: pre-wrap; /* To preserve formatting for plain text */
+            background-color: #f9f9f9;
+            border: 1px solid #ddd;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Files from Google Drive</h1>
+    <div>
+        @foreach($files as $file)
+            <div class="file-container">
+                <div class="file-name">{{ $file['name'] }}</div>
+                <div class="file-content">
+                    {!! nl2br(e($file['content'])) !!}
+                </div>
+            </div>
+        @endforeach
+    </div>
+</body>
+</html>
